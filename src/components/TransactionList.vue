091b39e92fbff833596f3ab47287b4ab7b239683
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-01-19 19:48:16 +0300

    Add logic to transaction list

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,28 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
       Cash <span>-$400</span>
       <button class="delete-btn">x</button>
     </li>
     <li class="plus">
       Paycheck <span>$900</span>
       <button class="delete-btn">x</button>
-    </li>
+    </li> -->
   </ul>
 </template>
+
+<script setup>
+const transactions = [
+  { id: 1, text: "Cash", amount: -400 },
+  { id: 2, text: "Paycheck", amount: 900 },
+];
+</script>
