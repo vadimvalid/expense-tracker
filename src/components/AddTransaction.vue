73f43001f69b0af68d9f6822c9d19dfdaaccf2c9
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-01-19 20:44:05 +0300

    Add logic to add transaction component

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,17 +1,32 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input v-model="text" type="text" id="text" placeholder="Enter text..." />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="number" id="amount" placeholder="Enter amount..." />
+      <input
+        v-model="amount"
+        type="text"
+        id="amount"
+        placeholder="Enter amount..."
+      />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const text = ref("");
+const amount = ref("");
+const onSubmit = () => {
+  console.log(text.value, amount.value);
+};
+</script>
